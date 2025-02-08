2c2f6159411cff05e9e3abe736b2350e163c6d03
Christian <khriskkh@gmail.com>
2025-02-08 09:54:26 -0500

    update style

--- js/scripts.js
+++ js/scripts.js
@@ -81,7 +81,7 @@ $(document).ready(function() {
         size: "small",
     });
     $("#idFriend").kendoTextBox({
-        placeholder: "ID Friend",
+        placeholder: "ID",
         size: "small",
     });
 
